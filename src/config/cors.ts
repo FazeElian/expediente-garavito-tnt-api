1f2a0a675e329b7db09eab9f4b34971684738287
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-04-17 11:40:45 -0500

    Enabling CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,18 @@
-// import { CorsOptions } from "cors";
+import { CorsOptions } from "cors";
 
-// export const CORSConfig : CorsOptions = {
-//     origin: function (origin, callback) {
-//         const whiteList = [process.env.VITE_URL]
+export const CORSConfig : CorsOptions = {
+    origin: function (origin, callback) {
+        const whiteList = [process.env.VITE_URL]
 
-//         if (process.argv[2] === "--api"){
-//             whiteList.push(undefined);
-//         }
+        if (process.argv[2] === "--api"){
+            whiteList.push(undefined);
+        }
 
-//         if (whiteList.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("CORS Error"));
-//         }
-//     }
-// }
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS Error"));
+            // callback(null, true);
+        }
+    }
+}
