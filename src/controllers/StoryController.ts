8ae5015e0d7dc11d8db70c820f53fecf0aef1f31
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-04-18 16:44:21 -0500

    add: Upload image feature to new story function

--- src/controllers/StoryController.ts
+++ src/controllers/StoryController.ts
@@ -26,29 +26,43 @@ export class StoryController {
     }
 
     static new = async (req: Request, res: Response) => {
-        try {
-            const story = new Story(req.body);
-            await story.save();
-
-            res.status(201).send("Story created sucessfully");
-        } catch (error) {
-            res.status(500).json({ error: "Error creating the story" })
-        }
-    }
-    static uploadImg = async (req: Request, res: Response) => {
         const form = formidable({
             multiples: false
         })
 
         try {
-            form.parse(req, (error, fields, files) => {
-                console.log(files.image[0].filepath)
-                cloudinary.uploader.upload(files.image[0].filepath, { public_id: uuid() }, async function (error, result) {
+            form.parse(req, (err, fields, files) => {
+                // console.log('Fields:', fields);
+                // console.log('Files:', files);
+                // console.log('Image file: ', files.image);
+                // console.log("Image empty: ", imageUploaded)
+
+                const imageUploaded = files.image
+
+                cloudinary.uploader.upload(imageUploaded[0].filepath, { public_id: uuid() }, async function (error, result) {
                     if (error) {
-                        res.status(500).json({ error: "Error uploading the image" })
+                        const error = new Error("Error uploading the image")
+                        return res.status(500).json({ error: error.message })
                     }
+
+                    const title = Array.isArray(fields.title) ? fields.title[0] : fields.title;
+                    const author = Array.isArray(fields.author) ? fields.author[0] : fields.author;
+                    const content = Array.isArray(fields.content) ? fields.content[0] : fields.content;
+
                     if (result) {
-                        console.log(result.url)
+                        const imageParsed = result.secure_url
+                        // res.json({ image: imageParsed })
+                        console.log(imageParsed)
+
+                        const story = new Story({
+                            title: title,
+                            author: author,
+                            content: content,
+                            image: imageParsed
+                        });
+
+                        await story.save();
+                        res.status(201).send("Story created sucessfully");
                     }
                 })
             })
@@ -56,4 +70,28 @@ export class StoryController {
             res.status(500).json({ error: "Error creating the story" })
         }
     }
+    // static uploadImg = async (req: Request, res: Response) => {
+    //     const form = formidable({
+    //         multiples: false
+    //     })
+
+    //     try {
+    //         form.parse(req, (error, fields, files) => {
+    //             // console.log(files.image[0].filepath)
+    //             cloudinary.uploader.upload(files.image[0].filepath, { public_id: uuid() }, async function (error, result) {
+    //                 if (error) {
+    //                     const error = new Error("Error uploading the image")
+    //                     return res.status(500).json({ error: error.message })
+    //                 }
+    //                 if (result) {
+    //                     req.story.image = result.secure_url
+    //                     await req.story.save()
+    //                     res.json({ image: result.secure_url })
+    //                 }
+    //             })
+    //         })
+    //     } catch (error) {
+    //         res.status(500).json({ error: "Error creating the story" })
+    //     }
+    // }
 }
